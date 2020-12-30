68a680ce9e30f2e4d55d28126e268f05a087d9f2
Грязева Анастасия Анатольевна <AAnGryazeva@sberbank.ru>
2020-12-30 17:22:43 +0300

    router & history

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Router from 'react-router-dom/es/Router';
+import { Route } from 'react-router-dom';
 import Game from './component/game/game';
 import styles from './App.module.scss';
+import history from './utils/history';
+
+const paths = {
+  game: '/game',
+  index: '/',
+  list: '/list',
+};
 
 const App = () => (
-  <div className={styles.container}>
-    <Game />
-  </div>
+  <Router history={history}>
+    <div className={styles.container}>
+      <Route
+        exact
+        path={paths.game}
+        component={() => (
+          <Game />
+        )}
+      />
+    </div>
+  </Router>
 );
 
 export default App;
