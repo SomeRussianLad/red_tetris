0a91e6adfdc3e3dddb3a92fcc7c81f4327d2971f
Грязева Анастасия Анатольевна <AAnGryazeva@sberbank.ru>
2021-01-05 12:16:46 +0300

    add list

--- src/client/src/component/joinGame/joinGame.jsx
+++ src/client/src/component/joinGame/joinGame.jsx
@@ -1,17 +1,33 @@
 import React, { useEffect } from 'react';
 
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import styles from './joinGame.module.scss';
+import { setListGame } from '../../middleware/storeStateMiddleWare';
 
-const JoinGame = () => {
+// eslint-disable-next-line react/prop-types,no-unused-vars
+const JoinGame = ({ list, dispatchList }) => {
   useEffect(() => {
-
+    // eslint-disable-next-line react/prop-types
+    if (!(list && list.length)) dispatchList();
   }, []);
 
   return (
     <div className={styles.container}>
-      <h1>Render</h1>
+      <ul>
+        {/* eslint-disable-next-line react/prop-types */}
+        {list && list.length && list.map((item) => (<li><NavLink to={`game/${item}`}>{item}</NavLink></li>))}
+      </ul>
     </div>
   );
 };
 
-export default JoinGame;
+const mapDispatchToProps = {
+  dispatchList: setListGame,
+};
+
+const mapStateToProps = (state) => ({
+  list: state.game.list,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(JoinGame);
