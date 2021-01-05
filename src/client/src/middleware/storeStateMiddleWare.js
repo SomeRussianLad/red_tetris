02d1aaed9cc15c06acd7636dcf709efd2413f77c
Грязева Анастасия Анатольевна <AAnGryazeva@sberbank.ru>
2021-01-05 12:16:46 +0300

    add list

--- src/client/src/middleware/storeStateMiddleWare.js
+++ src/client/src/middleware/storeStateMiddleWare.js
@@ -1,13 +1,20 @@
 import socketIOClient from 'socket.io-client';
 import handleErrors from '../utils/handleErrors';
-import { setGame } from '../store/actions/actionGame';
+import { setGame, setList } from '../store/actions/actionGame';
+
+const socket = socketIOClient();
 
 export const setNewGame = () => (dispatch) => {
-  const socket = socketIOClient();
   socket.on('new-game', (res) => {
     if (res.status !== 200) handleErrors(res);
     dispatch(setGame(res.id));
   });
   socket.emit('new-game');
 };
-export default setNewGame;
+
+export const setListGame = () => (dispatch) => {
+  socket.on('list-games', (res) => {
+    dispatch(setList(res.data));
+  });
+  socket.emit('list-games');
+};
