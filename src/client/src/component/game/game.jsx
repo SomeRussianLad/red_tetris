71ffb9d4d45fc9c298e35642c35c075c97a08ae0
Грязева Анастасия Анатольевна <AAnGryazeva@sberbank.ru>
2021-01-06 20:01:27 +0300

    add action

--- src/client/src/component/game/game.jsx
+++ src/client/src/component/game/game.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import styles from './game.module.scss';
-import { getMap, joinGame, startGame } from '../../middleware/storeStateMiddleWare';
+import {
+  actionGame, getMap, joinGame, startGame,
+} from '../../middleware/storeStateMiddleWare';
 
 // eslint-disable-next-line react/prop-types
 const Pixel = ({ color }) => (
@@ -15,9 +17,12 @@ const Pixel = ({ color }) => (
 const Game = ({
   // eslint-disable-next-line react/prop-types
   dispatchJoinGame, dispatchGetMap, dispatchStartGame, game, map,
+  // eslint-disable-next-line no-unused-vars,react/prop-types
+  dispatchAction,
 }) => {
   /* eslint-disable */
   const [startGame,setStartGame] = useState(false);
+  const [action, setAction] = useState('');
   const { id } = useParams();
 
   useEffect(() => {
@@ -31,6 +36,21 @@ const Game = ({
     console.log(map);
   },[map, startGame]);
 
+  useEffect(() =>{
+    document.addEventListener("keydown",handleKeyPress);
+  },[])
+
+  const handleKeyPress = ({code}) => {
+    const ACTION = {
+      ArrowLeft: 'left',
+      ArrowRight: 'right',
+      ArrowUp: 'rotate',
+      ArrowDown: 'down',
+      Space: 'drop',
+    }
+     dispatchAction({id: game, action: ACTION[code]})
+  }
+
   const start = () => {
     dispatchStartGame();
     setStartGame(true);
@@ -39,8 +59,10 @@ const Game = ({
   return (
     <div className={styles.container}>
       {id === game && (<button onClick={start}>START</button>)}
-      <div className={styles.containerMap}>
-        {map && map.field && map.field.length && map.field.map((str) => (
+      {map && map.isAlive &&
+        <div>
+        <div className={styles.containerMap}>
+        { map.field && map.field.length && map.field.map((str) => (
           <div className={styles.containerStr}>
             {str.map((color) => <Pixel color={color} />)}
           </div>
@@ -53,6 +75,7 @@ const Game = ({
           </div>
         ))}
       </div>
+    </div>}
     </div>
   );
 };
@@ -61,11 +84,13 @@ const Game = ({
   dispatchJoinGame: joinGame,
    dispatchStartGame: startGame,
    dispatchGetMap: getMap,
+   dispatchAction: actionGame,
 };
 
 const mapStateToProps = (state) => ({
   game: state.game.game,
   map: state.game.myMap,
+  otherMap: state.game.myMap,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(React.memo
--- src/client/src/middleware/storeStateMiddleWare.js
+++ src/client/src/middleware/storeStateMiddleWare.js
@@ -10,6 +10,9 @@ export const setNewGame = () => (dispatch) => {
   socket.on('new-game', (res) => {
     if (res.status !== 200) handleErrors(res);
     dispatch(setGame(res.id));
+    const id = res.id.split('-');
+    const player = `player-${id[1]}`;
+    dispatch(setPlayer(player));
     socket.off('new-game');
   });
   socket.emit('new-game');
@@ -25,6 +28,7 @@ export const setListGame = () => (dispatch) => {
 // eslint-disable-next-line no-unused-vars
 export const joinGame = (id) => (dispatch) => {
   socket.on('join-game', (res) => {
+    // console.log(res);
     if (res.status === 200) { dispatch(setPlayer(res.playerId)); }
     // id: "game-2y3PSr0iqVtmVp4nAAAB"
     // message: "Joined game session successfully"
@@ -38,7 +42,7 @@ export const joinGame = (id) => (dispatch) => {
 export const getMap = () => (dispatch) => {
   socket.on('new-state', (message) => {
     if (message.status === 200) {
-      console.log(message);
+      // console.log(message);
       dispatch(setMap(message.states));
     } else handleErrors(message);
   });
@@ -49,4 +53,13 @@ export const startGame = () => (dispatch) => {
     console.log(res);
   });
   socket.emit('start-game');
+};
+
+// eslint-disable-next-line no-unused-vars
+export const actionGame = ({ id, action }) => (dispatch) => {
+  socket.on('player-action', (res) => {
+    console.log(res);
+    if (res.status !== 200) { handleErrors(res); }
+  });
+  socket.emit('player-action', { id, action });
 };
